53a8fee54d9e0ae0702dedfa7746dde7a6f01be9
Mohd Taha Sharjeel <tahasharjeel@gmail.com>
2020-08-24 05:06:13 +0530

    Updates

---
+++ precache-manifest.3ac5ba5686e5f35e3d0fefd861af28f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dd13bb254b1dceb81706711b543e13",
+    "url": "/React-My-App/index.html"
+  },
+  {
+    "revision": "747ed65f312af6965086",
+    "url": "/React-My-App/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "a8f6b73d02849f86fbb5",
+    "url": "/React-My-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "747ed65f312af6965086",
+    "url": "/React-My-App/static/js/2.a39436c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-My-App/static/js/2.a39436c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f6b73d02849f86fbb5",
+    "url": "/React-My-App/static/js/main.92338fc2.chunk.js"
+  },
+  {
+    "revision": "72cdec1b975aff6f8cae",
+    "url": "/React-My-App/static/js/runtime-main.f419ccb5.js"
+  },
+  {
+    "revision": "c4127e0932b2e6cd8d626c3a0f71a2b6",
+    "url": "/React-My-App/static/media/circle-cropped.c4127e09.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-My-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
